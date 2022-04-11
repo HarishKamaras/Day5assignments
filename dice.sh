50948c0640087acedfbd828a3b416e07bcc86e1f
HarishKamaras <harishkamarasu@gmail.com>
2022-04-11 19:09:33 +0530

    success

---
+++ dice.sh
@@ -0,0 +1,20 @@
+diceno=$(( 1 + RANDOM%6 ))
+
+if(( $diceno == 1))
+then
+    echo "one"
+elif(( $diceno == 2))
+then
+    echo "two"
+elif(( $diceno == 3))
+then
+    echo "three"
+elif(( $diceno == 4))
+then
+    echo "four"
+elif(( $diceno == 5))
+then
+    echo "five"
+else
+    echo "six"
+fi
